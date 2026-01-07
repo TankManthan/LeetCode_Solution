80f4369b75cf98294814c45c165d449a0cc1a1d9
Tank Manthan <tank.manthan@gmail.com>
2026-01-07 17:01:01 +0530

    Update 1_twoSum.py

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -1,3 +1,18 @@
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
 from typing import List
 
 class Solution:
@@ -11,4 +26,5 @@ class Solution:
             
 obj = Solution()
 result =obj.twoSum([1,2,3,4,5,6],11)  # it will return index of target
+
 print(result)
