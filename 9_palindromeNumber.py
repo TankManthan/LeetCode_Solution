06584feb63b10fb724e5424fcd56f41a135bb52c
Tank Manthan <tank.manthan@gmail.com>
2026-01-13 10:44:08 +0530

    Update 9_palindromeNumber.py

--- 9_palindromeNumber.py
+++ 9_palindromeNumber.py
@@ -1,3 +1,19 @@
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         t=x
@@ -10,4 +26,5 @@ class Solution:
                 return True
             return False
 obj = Solution()
+
 print(obj.isPalindrome(121))
